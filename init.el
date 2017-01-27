59ef7dcea1ee8b2668d52a8174656936c4cd06fc
Eudes Lima <aeudes@gmail.com>
2017-01-27 13:18:12 -0300

    some cleaning

--- init.el
+++ init.el
@@ -3,6 +3,19 @@
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; here are the packages I'm using
+(setq package-list '(haskell-mode helm))
+
+;; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
 ;; loading custom.el
 (load (expand-file-name "custom.el" user-emacs-directory))
 
@@ -12,3 +25,21 @@
   (when (file-regular-p file)
     (load file)))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(package-selected-packages (quote (helm haskell-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
