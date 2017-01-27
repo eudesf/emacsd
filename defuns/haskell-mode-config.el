a78cf4287190c17e5a4a57ae340155f1ddfefbc7
Eudes Lima <aeudes@gmail.com>
2017-01-27 16:14:52 -0300

    updated haskell function call

--- defuns/haskell-mode-config.el
+++ defuns/haskell-mode-config.el
@@ -3,7 +3,7 @@
   '(haskell-process-auto-import-loaded-modules t)
   '(haskell-process-log t))
 (eval-after-load 'haskell-mode '(progn
-  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
   (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
   (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
   (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
